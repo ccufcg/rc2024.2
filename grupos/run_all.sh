3d5e33340e08ab117059e98793d8675e7870d1c8
Thiago <thiagonobrega@computacao.ufcg.eu.br>
2025-04-15 20:11:34 -0300

    scripts de avaliacao

---
+++ grupos/run_all.sh
@@ -0,0 +1,11 @@
+pkill -9 -f python
+./gerar_report.sh G1/projeto/ echo_server.py
+./gerar_report.sh G2/ servidor.py
+./gerar_report.sh G3/ serverTCP.py
+./gerar_report.sh G4/ servidor_ftcp.py
+./gerar_report.sh G5/ servidor_ftcp.py
+./gerar_report.sh G6/ server.py
+./gerar_report.sh G7/ftcp server.py
+./gerar_report.sh G8/ servidor_ftcp.py
+./gerar_report.sh G9/ servidor_ftcp.py
+./gerar_report.sh G10/src/ server_ftcp.py
